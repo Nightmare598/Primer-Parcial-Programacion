dcc8be7647d7ef18e8237ec87a8702995b64c781
Sebastian Morales Yabeta <masterrol001@hotmail.com>
2019-09-28 11:56:29 -0400

    2do Commit Creacion Modelo

---
+++ Morales/Sebastian/Models/Clown.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Sebastian.Models
+{
+    public enum TypeTricks { Sandia, Sofa, Sierra }
+    public class Clown
+    {
+        [Key]
+        public int ClownID { get; set; }
+        [Required]
+        [Display(Name = "Nombre Completo")]
+        [StringLength(24, ErrorMessage = "Entre 2 a 24 caracteres", MinimumLength = 2)]
+        public string NickName { get; set; }
+        [Required]
+        public TypeTricks Tricks { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Display(Name = "Cumpleaños")]
+        [DataType(DataType.Date)]
+        public DateTime Birthdate { get; set; }
+    }
+}
